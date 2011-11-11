e91ee1928df94fd1d0cbc2b9477a156cf4cf256e
Katie Horn <khorn@wikimedia.org>
2011-11-11 03:25:12 +0000

    Adding donate.wikimedia.org to the landing page filters (and slightly improved performance).

--- filters/lp-filter.c
+++ filters/lp-filter.c
@@ -2,18 +2,24 @@
 #include <stdio.h>
 #include <string.h>
 
-char burl[]="wikimediafoundation.org/";
+char url1[] ="wikimediafoundation.org/";
+char url2[] ="donate.wikimedia.org/";
 
 
 main() {
-	char line[10240];
-	char title[10240];
-	char *urlstart, *urlend;
+    	char line[65534];
+	char title[65534];
 	char *t = 0;
+        char *u = 0;
+        
+        //to cut down on the processing time: Guess first. 
+        //longest filter is 25 characters. 
+        //I'm allowing for one heck of a subdomain, here. 
+        int search_length = 75;
 
 	while (!feof(stdin)) {
 		char *r;
-		r=fgets(line, 10000, stdin);
+		r=fgets(line, 65534, stdin);
 
 		int pos=0;
 		t = line;
@@ -28,12 +34,25 @@ main() {
 		}
 		if (!t)
 			continue;
-		urlstart = t;
-		urlend = strstr(urlstart, " ");
-		strncpy(title, urlstart, urlend-urlstart);
-		title[urlend-urlstart]=0;
-		if (strstr(title, burl) )
-			printf("%s", line);
+
+		strncpy(title, t, search_length);
+		title[search_length]=0;
+                    
+		if (strstr(title, url1) || strstr(title, url2) ){
+                    u = strstr(title, " ");
+                    
+                    if (!u){ //no spaces, just do it. 
+                        printf("%s", line);
+                    } else {
+                        //make sure it was before the first space.
+                        t = strstr(title, url1);
+                        if (!t)
+                            t = strstr(title, url2);
+                        if ( (t) && t < u ) {
+                            printf("%s", line);
+                        }
+                    }
+                }
 
 	}
 
