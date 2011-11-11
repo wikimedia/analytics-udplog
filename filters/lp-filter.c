c5d54f65e97426d2d5d14d1d25d065425633a368
Katie Horn <khorn@wikimedia.org>
2011-11-11 20:07:29 +0000

    After confirming with metrics, made changes that filter out a whole lot of garbage. r102746

--- filters/lp-filter.c
+++ filters/lp-filter.c
@@ -2,20 +2,23 @@
 #include <stdio.h>
 #include <string.h>
 
-char url1[] ="wikimediafoundation.org/";
-char url2[] ="donate.wikimedia.org/";
-
+char * urls[] = {
+    "wikimediafoundation.org/",
+    "donate.wikimedia.org/",
+};
 
 main() {
     	char line[65534];
-	char title[65534];
 	char *t = 0;
-        char *u = 0;
         
-        //to cut down on the processing time: Guess first. 
-        //longest filter is 25 characters. 
-        //I'm allowing for one heck of a subdomain, here. 
-        int search_length = 75;
+        int i = 0;  
+        
+        //calculate these up-front.
+        int filtercount = sizeof(urls)/sizeof(*urls);
+        int url_length[filtercount];
+        for (i=0; i<filtercount; ++i){
+            url_length[i] = strlen(urls[i]);
+        }
 
 	while (!feof(stdin)) {
 		char *r;
@@ -24,7 +27,7 @@ main() {
 		int pos=0;
 		t = line;
 
-		while(pos++<8) {
+		while(pos++<7) {
 			if (!t)
 				continue;
 			t = strstr(t, " ");
@@ -34,24 +37,21 @@ main() {
 		}
 		if (!t)
 			continue;
-
-		strncpy(title, t, search_length);
-		title[search_length]=0;
-                    
-		if (strstr(title, url1) || strstr(title, url2) ){
-                    u = strstr(title, " ");
-                    
-                    if (!u){ //no spaces, just do it. 
-                        printf("%s", line);
-                    } else {
-                        //make sure it was before the first space.
-                        t = strstr(title, url1);
-                        if (!t)
-                            t = strstr(title, url2);
-                        if ( (t) && t < u ) {
-                            printf("%s", line);
-                        }
-                    }
+                
+                t = strstr(t, "://");
+                if (!t)
+                        continue;
+                t += 3;
+                
+                int found = 0;
+                for (i = 0; i < filtercount; ++i) {
+                   if (strncmp(t, urls[i], url_length[i]) == 0) {
+                       found = 1;
+                       break;
+                   }
+                }
+                if (found) {
+                    printf("%s", line);
                 }
 
 	}
