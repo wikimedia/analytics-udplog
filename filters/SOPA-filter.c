e746362bb6d368908efc0bf297c3452b803ad6b4
Ben Hartshorne <bhartshorne@wikimedia.org>
2012-01-17 20:11:33 +0000

    retabbing to eliminate mixed spaces and tabs

--- filters/SOPA-filter.c
+++ filters/SOPA-filter.c
@@ -3,21 +3,21 @@
 #include <string.h>
 
 char * urls[] = {
-    "en.wikipedia.org/wiki/Special:CongressLookup",
+	"en.wikipedia.org/wiki/Special:CongressLookup",
 };
 
 main() {
-    	char line[65534];
+	char line[65534];
 	char *t = 0;
-        
-        int i = 0;  
-        
-        //calculate these up-front.
-        int filtercount = sizeof(urls)/sizeof(*urls);
-        int url_length[filtercount];
-        for (i=0; i<filtercount; ++i){
-            url_length[i] = strlen(urls[i]);
-        }
+
+	int i = 0;  
+
+	//calculate these up-front.
+	int filtercount = sizeof(urls)/sizeof(*urls);
+	int url_length[filtercount];
+	for (i=0; i<filtercount; ++i){
+		url_length[i] = strlen(urls[i]);
+	}
 
 	while (!feof(stdin)) {
 		char *r;
@@ -36,22 +36,22 @@ main() {
 		}
 		if (!t)
 			continue;
-                
-                t = strstr(t, "://");
-                if (!t)
-                        continue;
-                t += 3;
-                
-                int found = 0;
-                for (i = 0; i < filtercount; ++i) {
-                   if (strncmp(t, urls[i], url_length[i]) == 0) {
-                       found = 1;
-                       break;
-                   }
-                }
-                if (found) {
-                    printf("%s", line);
-                }
+
+		t = strstr(t, "://");
+		if (!t)
+			continue;
+		t += 3;
+
+		int found = 0;
+		for (i = 0; i < filtercount; ++i) {
+			if (strncmp(t, urls[i], url_length[i]) == 0) {
+				found = 1;
+				break;
+			}
+		}
+		if (found) {
+			printf("%s", line);
+		}
 
 	}
 
