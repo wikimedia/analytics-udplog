273d88cdd22ff9894a2fbef7e54c9af5c0fbf2ee
Diederik van Liere <dvanliere@wikimedia.org>
2012-04-19 17:49:03 +0000

    ip_mreqn is not compatible with OS X, added alternative for OSX.

--- srclib/Socket.cpp
+++ srclib/Socket.cpp
@@ -4,13 +4,27 @@ int UDPSocket::JoinMulticast(const IPAddress & addr) {
 	int level, optname;
 	void *optval;
 	socklen_t optlen;
-	struct ip_mreqn mreq4;
+    
+    #ifdef __APPLE__
+        	// NOTE: The ip_mreqn structure is not supported on Mac OS X,
+        	// NOTE: so we use ip_mreq and ignore the interface parameter
+		struct ip_mreq mreq4;
+	#elif __unix__
+		struct ip_mreqn mreq4;
+	#endif
+	
 	struct ipv6_mreq mreq6;
 
 	if (addr.GetType() == AF_INET) {
 		addr.CopyBinaryData(&mreq4.imr_multiaddr.s_addr);
-		IPAddress::any4.CopyBinaryData(&mreq4.imr_address);
-		mreq4.imr_ifindex = 0;
+		
+		#ifdef __APPLE__
+			IPAddress::any4.CopyBinaryData(&mreq4.imr_multiaddr);	
+		#elif __unix__
+			IPAddress::any4.CopyBinaryData(&mreq4.imr_address);
+			mreq4.imr_ifindex = 0;
+		#endif
+		
 		level = IPPROTO_IP;
 		optname = IP_ADD_MEMBERSHIP;
 		optval = &mreq4;
